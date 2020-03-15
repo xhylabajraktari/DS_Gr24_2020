ac9a0413c3595a210cf017442745deac611fe5b0
xhylabajraktari <59016369+xhylabajraktari@users.noreply.github.com>
2020-03-15 16:00:39 +0000

    Rail-Fence
    Enkriptimi edhe dekriptimi permes komandes Rail Fence

--- RailFence/src/komanda2/RailFenceCipher.java
+++ RailFence/src/komanda2/RailFenceCipher.java
@@ -1,36 +1,40 @@
-package ENCODER;
+package komanda2;
 import java.util.Scanner;
 public class RailFenceCipher {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter text : ");
+		System.out.print("Shkruani tekstin : ");
 		String plainText=in.nextLine();
-		System.out.print("Enter the key: ");
+		System.out.print("Shkruani nr. e qelsit: ");
 		int key =in.nextInt();
-		//System.out.println(encryption(plainText,key));
 		String en=encryption(plainText,key);
+		System.out.print("Teksti i enkriptuar : ");
 		System.out.println(en);
-		System.out.println(decryption(en,key));
+		String den=decryption(en,key);
+		System.out.print("Teksti i dekriptuar : ");
+		System.out.println(den);
+		
 		
 	}
  static String encryption(String text,int key) {
 	 String encryptedText="";
-	 boolean check=false;
+	 boolean check=false;   //Kontrollimi nese pozita e shkronjes eshte poshte ose larte
 	 int j=0;
-	 int row=key;
-	 int col=text.length();
+	 int row=key;    //Rreshtat jane aq sa jan key
+	 int col=text.length();   //Kolonatt jane aq sa eshte teksti i gjate
 	 char[][] a= new char[row][col];
 	 for(int i=0;i<col;i++) {
-		 if(j==0||j==key-1)
-			 check = !check;
-		 a[j][i]=text.charAt(i);
+		 if(j==0||j==key-1)    //Shikimi nese j eshte ne fillim te row ose ne fund te row
+			 check = !check;   //Ndrrimi check sepse nese eshte top do te shkoj down 
+		 a[j][i]=text.charAt(i);//Shtimi i shkronjave ne poziten e tyre 
 		 if(check)
-			 j++;
+			 j++;//e rrisim j
 		 else 
-			j--;
+			j--;//e zvogelojme j
 		
 	 }
+	 //Leximi rresht per rresht
 	 for(int i=0;i<row;i++) {
 		 for(int k=0;k<col;k++) {
 		if(a[i][k]!=0)
@@ -38,7 +42,7 @@ public class RailFenceCipher {
 		 }
 		 
 	 }
-	 
+	 // Paraqitja e matrices
 	 for(int i=0;i<row;i++) {
 		 for(int k=0;k<col;k++) {
 		 System.out.print(a[i][k]+"");
@@ -53,6 +57,7 @@ public class RailFenceCipher {
 	 int j=0;
 	 int row=key;
 	 int col=text.length();
+	 //Na duhet matrica
 	 char[][] a= new char[row][col];
 	 for(int i=0;i<col;i++) {
 		 if(j==0||j==key-1)
@@ -72,12 +77,8 @@ public class RailFenceCipher {
 	 if(a[i][k]=='*' && index<col) {
 		 a[i][k]=text.charAt(index++);
 	 }}}
-   for(int i=0;i<row;i++) {
-	   for (int k=0;k<col;k++ ) {
-		   System.out.print(a[i][k]+" ");
-	   }
-		System.out.println();
-	}
+ 
+	 //Paraqitja e tekstit te dekriptuar
  j=0;
 	 for(int i=0;i<col;i++) {
 		 if(j==0||j==key-1)
