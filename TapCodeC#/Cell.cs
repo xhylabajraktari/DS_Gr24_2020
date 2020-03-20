700e48258b42b0049730ee2a9931c8b28ecb138a
Krasniqi550 <59016369+xhylabajraktari@users.noreply.github.com>
2020-03-20 22:43:38 +0000

    /

---
+++ TapCodeC#/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TapCode
+{
+    class Cell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+        public char Letter { get; set; }
+
+        public Cell(int x, int y, char letter)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Letter = letter;
+        }
+        public Cell()
+        {
+
+        }
+
+    }
+}
